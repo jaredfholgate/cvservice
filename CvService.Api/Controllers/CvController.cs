69d988c081b720e1f71116bfd66e7f13cf190d85
jaredfholgate <jaredfholgate@gmail.com>
2019-01-19 16:16:27 -0500

    Make casing consistent on CV controller route.

--- CvService.Api/Controllers/CvController.cs
+++ CvService.Api/Controllers/CvController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CvService.Api.Controllers
 {
-  [Route("[controller]")]
+  [Route("cv")]
   [ApiController]
   public class CvController : BaseController
   {
